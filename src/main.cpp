6b334f516ae1025ffd23daaddb8aefffaaf3397a
pysojic <pysojic@uchicago.edu>
2025-10-20 01:52:17 +0200

    first version

--- src/main.cpp
+++ src/main.cpp
@@ -1,78 +1,133 @@
+// g++ -std=c++17 -O2 poll_chat.cpp -o poll_chat
+#include <algorithm>
+#include <cerrno>
+#include <csignal>
+#include <cstring>
+#include <fcntl.h>
 #include <iostream>
-#include <sys/types.h>
-#include <sys/socket.h>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
 #include <netdb.h>
-#include <unistd.h>
 #include <netinet/in.h>
-#include <arpa/inet.h>
+#include <poll.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
 
-#include <print>
+static void die(const char* msg) { std::perror(msg); std::exit(1); }
 
-int main(int argc, const char** argv)
-{
-    if (argc != 2) 
-    {
-        std::println("usage: showip hostname");
-        return -1;
-    }
-    
-    int status;
-    addrinfo hints{}; // hints for the getaddrinfo() func
-    addrinfo* res; // resulting linked-list of the getaddrinfo()
-    addrinfo* p; // pointer to iterate over the above linked-list
+static void set_nonblock(int fd) {
+    int fl = fcntl(fd, F_GETFL, 0);
+    if (fl == -1 || fcntl(fd, F_SETFL, fl | O_NONBLOCK) == -1) die("fcntl");
+}
 
-    hints.ai_family = AF_UNSPEC; // don't care if it is iPv4 or iPv6
-    hints.ai_socktype = SOCK_STREAM; // TCP stream socket
-    hints.ai_flags = AI_PASSIVE; // fill IP for me
+int main(int argc, char** argv) {
+    if (argc != 2) { std::cerr << "usage: " << argv[0] << " <port>\n"; return 1; }
 
-    if ((status = getaddrinfo(argv[1], "3490", &hints, &res)) != 0)
-    {
-        std::println("gai error: {}", gai_strerror(status));
+    std::signal(SIGPIPE, SIG_IGN); // portable way to avoid SIGPIPE on send()
+
+    addrinfo hints{}; hints.ai_family = AF_UNSPEC; hints.ai_socktype = SOCK_STREAM; hints.ai_flags = AI_PASSIVE;
+    addrinfo* res = nullptr;
+    if (int rc = getaddrinfo(nullptr, argv[1], &hints, &res); rc != 0) {
+        std::cerr << "getaddrinfo: " << gai_strerror(rc) << "\n"; return 1;
     }
 
-    // Print each addresses of the host given in the command line
-    for(p = res; p != nullptr; p = p->ai_next) 
-    {
-        const void* addr = nullptr;
-        const char* ipver = nullptr;
-
-        if (p->ai_family == AF_INET) 
-        {
-            auto* ipv4 = reinterpret_cast<sockaddr_in*>(p->ai_addr);
-            addr = &ipv4->sin_addr;
-            ipver = "IPv4";
-        } 
-        else if (p->ai_family == AF_INET6) 
-        {
-            auto* ipv6 = reinterpret_cast<sockaddr_in6*>(p->ai_addr);
-            addr = &ipv6->sin6_addr;
-            ipver = "IPv6";
-        } 
-        else 
-        {
-            continue; // skip unknown families
-        }
-
-        std::array<char, INET6_ADDRSTRLEN> ipstr{};  // big enough for v4 and v6
-
-        // converts the IP address to a string
-        if (!inet_ntop(p->ai_family, addr, ipstr.data(), ipstr.size())) 
-        {
-            // handle error if you want (errno has the reason)
-            continue;
-        }
-
-        std::println("  {}: {}", ipver, ipstr.data());
+    int lfd = -1;
+    for (addrinfo* p = res; p; p = p->ai_next) {
+        lfd = ::socket(p->ai_family, p->ai_socktype, p->ai_protocol);
+        if (lfd == -1) continue;
+        int yes = 1;
+        setsockopt(lfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(yes));
+#ifdef SO_REUSEPORT
+        setsockopt(lfd, SOL_SOCKET, SO_REUSEPORT, &yes, sizeof(yes));
+#endif
+        if (::bind(lfd, p->ai_addr, p->ai_addrlen) == 0) break;
+        ::close(lfd); lfd = -1;
     }
-
-    int sockfd; // Socket file descriptor
-    if ((sockfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol)) == -1)
-    {
-        std::println("Socket error");
-    }
-
-    bind(sockfd, res->ai_addr, res->ai_addrlen);
-
-
     freeaddrinfo(res);
+    if (lfd == -1) die("bind");
+    if (::listen(lfd, SOMAXCONN) == -1) die("listen");
+    set_nonblock(lfd);
+
+    std::vector<pollfd> pfds;
+    pfds.push_back({lfd, POLLIN, 0}); // index 0 = listening socket
+
+    std::unordered_map<int, std::string> outbuf; // fd -> pending bytes
+
+    auto add_client = [&](int cfd) {
+        set_nonblock(cfd);
+        pfds.push_back({cfd, POLLIN, 0});
+        outbuf.try_emplace(cfd);
+    };
+    auto remove_client = [&](size_t idx) {
+        int cfd = pfds[idx].fd;
+        ::close(cfd);
+        outbuf.erase(cfd);
+        pfds[idx] = pfds.back();
+        pfds.pop_back();
+    };
+
+    std::cout << "listening on port " << argv[1] << " ...\n";
+    char buf[8192];
+
+    for (;;) {
+        if (::poll(pfds.data(), pfds.size(), -1) < 0) {
+            if (errno == EINTR) continue;
+            die("poll");
+        }
+
+        // 1) Accept new clients
+        if (pfds[0].revents & POLLIN) {
+            for (;;) {
+                sockaddr_storage ss{}; socklen_t slen = sizeof(ss);
+                int cfd = ::accept(lfd, reinterpret_cast<sockaddr*>(&ss), &slen);
+                if (cfd == -1) {
+                    if (errno == EAGAIN || errno == EWOULDBLOCK) break;
+                    std::perror("accept"); break;
+                }
+                add_client(cfd);
+            }
+        }
+
+        // 2) Service clients (iterate backward so removal is O(1))
+        for (size_t i = pfds.size(); i-- > 1;) {
+            auto& p = pfds[i];
+            if (p.revents & (POLLHUP | POLLERR | POLLNVAL)) { remove_client(i); continue; }
+
+            // Readable: receive and broadcast
+            if (p.revents & POLLIN) {
+                for (;;) {
+                    ssize_t n = ::recv(p.fd, buf, sizeof(buf), 0);
+                    if (n > 0) {
+                        // Broadcast to others: append to their out buffers
+                        for (size_t j = 1; j < pfds.size(); ++j) {
+                            int ofd = pfds[j].fd;
+                            if (ofd == p.fd) continue;
+                            auto& q = outbuf[ofd];
+                            q.append(buf, static_cast<size_t>(n));
+                            pfds[j].events |= POLLOUT; // ask poll to notify when writable
+                        }
+                    } else if (n == 0) { remove_client(i); break; } // peer closed
+                    else {
+                        if (errno == EAGAIN || errno == EWOULDBLOCK) break;
+                        std::perror("recv"); remove_client(i); break;
+                    }
+                }
+            }
+
+            // Writable: flush pending data
+            if ((p.revents & POLLOUT) && !outbuf[p.fd].empty()) {
+                std::string& q = outbuf[p.fd];
+                ssize_t m = ::send(p.fd, q.data(), q.size(), 0);
+                if (m > 0) {
+                    q.erase(0, static_cast<size_t>(m));
+                    if (q.empty()) p.events &= ~POLLOUT;
+                } else if (m < 0 && !(errno == EAGAIN || errno == EWOULDBLOCK)) {
+                    std::perror("send"); remove_client(i);
+                }
+            }
+        }
+    }
 }
